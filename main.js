c72ced6108bc2a0306c84c5ad0109d2ae53919f0
Boot-Linux-Port-0418 <Vous@exemple.com>
2019-03-01 15:50:29 +0100

    List Rendering

--- main.js
+++ main.js
@@ -2,7 +2,18 @@ var app = new Vue({
     el: '#app',
     data: { product: 'Socks',
             image: './assets/vmSocks-green-onWhite.jpg',
-            // inStock: true
-            inventory: 100
+            inStock: true,
+            // inventory: 100,
+            details: ["80% cotton", "20% polyester", "Gender-neutral"],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: "green"
+                },
+                {
+                    variantId: 2235,
+                    variantColor: "blue"
+                }
+            ]
     }
 })
