8b5d572c7ddb3c333ff10fef61cdcd0467855586
Boot-Linux-Port-0418 <Vous@exemple.com>
2019-03-02 23:09:32 +0100

    Computed Properties

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 var app = new Vue({
     el: '#app',
-    data: { product: 'Socks',
-            image: './assets/vmSocks-green-onWhite.jpg',
-            inStock: false,
+    data: { 
+            brand: 'Vue Mastery',
+            product: 'Socks',
+            selectedVariant: 0,
+            // image: './assets/vmSocks-green-onWhite.jpg',
+            // inStock: false,
             // inventory: 100,
             details: ["80% cotton", "20% polyester", "Gender-neutral"],
             variants: [
                 {
                     variantId: 2234,
                     variantColor: "green",
-                    variantImage: './assets/vmSocks-green-onWhite.jpg'
+                    variantImage: './assets/vmSocks-green-onWhite.jpg',
+                    variantQuantity: 10
                 },
                 {
                     variantId: 2235,
                     variantColor: "blue",
-                    variantImage: './assets/vmSocks-blue-onWhite.jpg'
+                    variantImage: './assets/vmSocks-blue-onWhite.jpg',
+                    variantQuantity: 0
                 }
             ],
             cart: 0
@@ -23,9 +28,13 @@ var app = new Vue({
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index
+            console.log(index)
         }
+        // updateProduct(variantImage) {
+        //     this.image = variantImage
+        // }
 
         // addToCart: function() {
         //     this.cart += 1
@@ -33,5 +42,16 @@ var app = new Vue({
         // updateProduct: function(variantImage) {
         //     this.image = variantImage
         // }
-    }
+    },
+    computed: {
+        title() {
+            return this.brand + '' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        }
+    },
 })
